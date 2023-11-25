d210882a6d5b1cb2bcf4b90e4e0d3537f708fced
Tainá Wandermurem Conti <87378037+tainaconti@users.noreply.github.com>
2023-11-24 21:51:00 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -3,15 +3,17 @@ const tesults = require('cypress-tesults-reporter');
 
 const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE0NTk4ZTJlLTYxNGEtNGY5NC1hY2EyLTZjMzM4NjYyMzQ4OS0xNzAwODcxMTIxMjMzIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiNjNiYTMxZTctNTIzMy00YmMxLTljYWYtODlmNDU4NmNiNzhhIiwidHlwZSI6InQifQ.edPyCkvnZ7NZ3tn_dV0paDHW2uEO_ix-PQvFsYxsYPU'
 
-cypress.run({
-  // specs to run here
-})
-.then((results) => {
-  const args = {
-    target: 'TOKEN',
-  }
-  tesults.results(results, args);
-})
-.catch((err) => {
- console.error(err)
-})
+const cypressOptions = {
+  spec: '**/*.spec.ts', 
+};
+
+cypress.run(cypressOptions)
+  .then((results) => {
+    const args = {
+      target: TOKEN,
+    };
+    tesults.results(results, args);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
